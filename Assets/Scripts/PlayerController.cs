660f707659821ce3377ea7680d9e474d565a478e
Sayoshi1126 <buromasa1126@gmail.com>
2021-10-24 15:49:51 +0900

    プロジェクト作成

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        control();
+    }
+
+    void control()
+    {
+
+    }
+}
